58cd298d87a7a1c4653d769e2bd83121d48ffaba
AliqQim <hnero@mail.ru>
2020-04-19 17:03:57 +0600

    добавил тесты

---
+++ TextUtils/TextUtilsTests/SnoskiConverterTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using snoski;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snoski.Tests
+{
+    public class SnoskiConverterTests
+    {
+        [Fact()]
+        public void ConvertTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
