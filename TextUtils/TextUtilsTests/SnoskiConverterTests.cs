f857e112ca80d5227ccc171d90b5a22403bc2b09
AliqQim <hnero@mail.ru>
2020-04-25 14:09:36 +0600

    чето я не докоммитил в прошлый раз

--- TextUtils/TextUtilsTests/SnoskiConverterTests.cs
+++ TextUtils/TextUtilsTests/SnoskiConverterTests.cs
@@ -3,6 +3,7 @@ using snoski;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using CommonUtils;
 
 namespace snoski.Tests
 {
@@ -11,6 +12,8 @@ namespace snoski.Tests
         [Fact()]
         public void ConvertTest()
         {
+            string s = FileUtils.GetPathToCurrentAssemblyProjectFolder();
+
             Assert.True(false, "This test needs an implementation");
         }
     }
