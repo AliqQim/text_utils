08537c210089f55269e3a9192db1cc5776d22289
AliqQim <hnero@mail.ru>
2020-06-14 19:01:09 +0600

    начал прописывать очистку - какого-то хера на ровном месте не срабатывает

--- TextUtils/TextUtils/Fb2ToTextConverter.cs
+++ TextUtils/TextUtils/Fb2ToTextConverter.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace Fb2ToReadAloudText
 {
-    class Fb2ToTextConverter
+    public class Fb2ToTextConverter
     {
-        Remove
+        public string Convert(string fb2Xml)
+        {
+
+            var doc = new XmlDocument();
+            doc.LoadXml(fb2Xml);
+            var author = doc.SelectSingleNode("/FictionBook/description/title-info/author");
+
+            //var root = XElement.Parse(fb2Xml);
+            //var author = root.XPathSelectElement("/FictionBook");
+            //string authorName = $"{author.Element("last-name")} {author.Element("first-name")} {author.Element("middle-name")}"
+            //    .Trim();
+
+            throw new NotImplementedException();    //**!!
+        }
     }
 }
---
+++ TextUtils/TextUtilsTests/Fb2ToTextConverterTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using Fb2ToReadAloudText;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using CommonUtils;
+
+namespace Fb2ToReadAloudText.Tests
+{
+    public class Fb2ToTextConverterTests
+    {
+        [Fact()]
+        public void ConvertTest()
+        {
+            string inputFileName = "input_fullXml.xml";
+
+            string input = File.ReadAllText(
+                Path.Combine(
+                    FileUtils.GetPathToCurrentAssemblyCsprojFolder(),
+                    "Fb2ToTextConverterTest",
+                    inputFileName)
+                );
+
+            var target = new Fb2ToTextConverter();
+            string res = target.Convert(input);
+
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
