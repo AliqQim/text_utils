dedf8c08c87b1fc556f37cffd511b4b69010b365
AliqQim <hnero@mail.ru>
2020-06-15 21:03:11 +0600

    считывание работает

--- TextUtils/TextUtils/Fb2ToTextConverter.cs
+++ TextUtils/TextUtils/Fb2ToTextConverter.cs
@@ -14,14 +14,21 @@ namespace Fb2ToReadAloudText
 
             var doc = new XmlDocument();
             doc.LoadXml(fb2Xml);
-            var author = doc.SelectSingleNode("/FictionBook/description/title-info/author");
+
+            var nsMgr = new XmlNamespaceManager(doc.NameTable);
+            nsMgr.AddNamespace("m", "http://www.gribuser.ru/xml/fictionbook/2.0");  //иначе не XPath теги не видит
+
+            var author = doc.SelectSingleNode("/m:FictionBook/m:description/m:title-info/m:author", nsMgr);
+
+
+            throw new NotImplementedException();    //**!!
 
             //var root = XElement.Parse(fb2Xml);
             //var author = root.XPathSelectElement("/FictionBook");
             //string authorName = $"{author.Element("last-name")} {author.Element("first-name")} {author.Element("middle-name")}"
             //    .Trim();
 
-            throw new NotImplementedException();    //**!!
+            
         }
     }
 }
